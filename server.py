f4f547462fa930cd9980c22a07184d88c11aaa58
k.buraya <k.buraya@kburaya-nb.local>
2018-10-19 22:59:49 +0300

    Server simplt REST protocol added

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import random
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class User(Resource):
+    def get_mbti(self, username):
+        e_i = 'E' if random.randint(0, 1) == 0 else 'I'
+        s_n = 'S' if random.randint(0, 1) == 0 else 'N'
+        t_f = 'T' if random.randint(0, 1) == 0 else 'F'
+        j_p = 'J' if random.randint(0, 1) == 0 else 'P'
+
+        mbti = {
+            'Extraversion/Intraversion': e_i,
+            'Sensing/Intuition': s_n,
+            'Thinking/Feeling': t_f,
+            'Judjing/Perception': j_p
+        }
+
+        return mbti
+
+    def get(self, name):
+            # download 10 images from instagram
+            mbti = self.get_mbti(name)
+            return mbti
+
+api.add_resource(User, "/user/<string:name>")
+
+app.run(debug=True)
